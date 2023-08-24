5ce5e1ea6d49900e016b648c504fa8c462fb5a43
JongMinCh0i <wonderland314592@gmail.com>
2023-08-24 22:07:09 +0900

    1.8 속성 값 설정

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -83,6 +83,10 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 
     @Data
     static class User {
